639ea8de55d443690871272546fc70e41f9fa965
robuGdada <paudelronish@gmail.com>
2025-04-29 16:52:34 +0545

    Add Nuxt configuration, package.json, and .gitignore files

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// nuxt.config.ts
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
